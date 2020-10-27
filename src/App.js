0bd8a2ac388ccd00a1c20c410ea029612e6c55e8
Andrew Dalba <andrewdalba@gmail.com>
2020-10-26 20:05:39 -0400

    create navbar and main page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    
     <Router>
-      <NavBar />
-      <Home/>
-      <About />
-      <Portfolio />
-      <Contact />
-      <div>
+    <Navbar/>
+    <Home/>
+    <div>
         <Route exact path="/home" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-      </div>
+    </div>
     </Router>
+
+    // <Router>
+    //   <NavBar />
+    //   <Home/>
+    //   <About />
+    //   <Portfolio />
+    //   <Contact />
+    //   <div>
+    //     <Route exact path="/home" component={Home} />
+    //     <Route exact path="/about" component={About} />
+    //     <Route exact path="/portfolio" component={Portfolio} />
+    //     <Route exact path="/contact" component={Contact} />
+    //   </div>
+    // </Router>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,32 @@
 import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 
 function Home() {
     return (
         <Fragment>
-            <section id="home" className="homepage">
-            
-            <img src="assets/images/ktlnn-profile.png" alt="Katelynn" class="img-fluid" />
-            <div className="typewriter">
-            <h1 className="header">Hi, I'm Katelynn Nguyen</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12">
+                        <div className="page-header">
+                            <h1>About Me</h1>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-12 col-md-6 col-lg-3">
+                                <img src="./assets/Me.jpg" alt="Andrew D'Alba" className="img-thumbnail" />
+                            </div>
+                            <div className="col-sm-12 col-md-6 col-lg-9">
+                                <p className="text-justify">
+                                    I am a student of the Rutgers Coding Bootcamp. In the spring of 2019
+                                    I graduated from the University of Hartford with a B.S. in Audio
+                                    Engineering Technology and an A.S. in Electrical Engineering
+                                    Technology. I like to play the guitar and my favorite color is purple.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p className="description">I am an aspiring full stack web developer that is self-motivated and hard-working. 
-            </p>
-            </section>
-
         </Fragment>
     )
 }
